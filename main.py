c4c313368a3ae8477679f9aeb1376af2a03996b0
Franq <Franqzadi@gmail.com>
2026-03-13 09:18:27 -0600

    refactor: separar logica en funciones pequeñas con proposito unico

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import random
 
-def iniciar_juego():
-    numero_secreto = random.randint(1, 20)
-    intento_usuario = 0
-    intentos_totales = 0
+def mostrar_instrucciones():
+    print("--- BIENVENIDO AL JUEGO ---")
     print("Adivina el número entre 1 y 20")
+
+def procesar_intento(intento, objetivo):
+    if intento < objetivo:
+        print("Muy bajo")
+        return False
+    elif intento > objetivo:
+        print("Muy alto")
+        return False
+    print("¡Correcto!")
+    return True
+
+def iniciar_juego():
+    mostrar_instrucciones()
+    numero_secreto = random.randint(1, 20)
+    intentos = 0
+    adivinado = False
     
-    while intento_usuario != numero_secreto:
-        intento_usuario = int(input("Ingresa tu intento: "))
-        intentos_totales += 1
-        if intento_usuario < numero_secreto:
-            print("Muy bajo")
-        elif intento_usuario > numero_secreto:
-            print("Muy alto")
-        elif intento_usuario == numero_secreto:
-            print("¡Correcto!")
-        print("Número de intentos:", intentos_totales)
+    while not adivinado:
+        intento = int(input("Ingresa tu intento: "))
+        intentos += 1
+        adivinado = procesar_intento(intento, numero_secreto)
+        print(f"Intentos: {intentos}")
 
 iniciar_juego()
